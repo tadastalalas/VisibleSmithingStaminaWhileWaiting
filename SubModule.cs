8895ed9ac9030e4bfa06dc23f6801a5cca07568f
Tadas Talalas <tadastalalas@gmail.com>
2024-12-17 22:26:27 +0200

    Add project files.

---
+++ SubModule.cs
@@ -0,0 +1,100 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.CampaignBehaviors;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+
+namespace SmithingStaminaDisplayedWhileWaiting
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            base.OnGameStart(game, gameStarterObject);
+            Campaign campaign = Campaign.Current;
+            bool flag = campaign != null && campaign.GameMode == CampaignGameMode.Campaign;
+            if (flag)
+            {
+                CampaignGameStarter campaignGameStarter = (CampaignGameStarter)gameStarterObject;
+                campaignGameStarter.AddBehavior(new SSDWhileWaiting());
+            }
+        }
+
+        public class SSDWhileWaiting : CampaignBehaviorBase
+        {
+            public override void RegisterEvents()
+            {
+                CampaignEvents.HourlyTickEvent.AddNonSerializedListener(this, HourlyTick);
+            }
+
+            public override void SyncData(IDataStore dataStore)
+            {
+                
+            }
+
+            private bool _canStaminaMessageBeDisplayed = false;
+
+            private void HourlyTick()
+            {
+                if (!IsHeroInAnySettlement())
+                    return;
+
+                if (!DoesHeroHasMaxStamina() && !IsMessageReadyToBeDisplayed())
+                {
+                    SetMessageToBeDisplayed(true);
+                    return;
+                }
+
+                if (DoesHeroHasMaxStamina() && IsMessageReadyToBeDisplayed())
+                    DisplayStaminaMessage();
+            }
+
+            private bool IsHeroInAnySettlement() => Settlement.CurrentSettlement != null;
+
+            private bool DoesHeroHasMaxStamina()
+            {
+                var maxMainHeroStamina = Campaign.Current.GetCampaignBehavior<CraftingCampaignBehavior>().GetMaxHeroCraftingStamina(Hero.MainHero);
+                var currentMainHeroStamina = Campaign.Current.GetCampaignBehavior<CraftingCampaignBehavior>().GetHeroCraftingStamina(Hero.MainHero);
+                if (currentMainHeroStamina >= maxMainHeroStamina)
+                    return true;
+                return false;
+            }
+
+            private bool IsMessageReadyToBeDisplayed()
+            {
+                if (_canStaminaMessageBeDisplayed)
+                    return true;
+                return false;
+            }
+
+            private void SetMessageToBeDisplayed(bool flag) => _canStaminaMessageBeDisplayed = flag;
+
+            private void DisplayStaminaMessage()
+            {
+                InformationManager.DisplayMessage(new InformationMessage(new TextObject("Main hero stamina reached 100%").ToString()));
+                SetMessageToBeDisplayed(false);
+            }
+        }
+    }
+}
